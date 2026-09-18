312ee84b39e066a11a8ed41c9594210c59ee9bfc
Luigi <luigivibias11@gmail.com>
2026-09-17 21:47:28 -0300

    Ajustes Finos

--- src/main/java/org/testepratico/Funcionario.java
+++ src/main/java/org/testepratico/Funcionario.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.testepratico;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
--- src/main/java/org/testepratico/Pessoa.java
+++ src/main/java/org/testepratico/Pessoa.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.testepratico;
 
 import java.time.LocalDate;
 import java.time.Period;
